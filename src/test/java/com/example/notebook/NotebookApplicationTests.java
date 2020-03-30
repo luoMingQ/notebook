3fe8654e25d7ddb413f90cf981fa09623dec3f1d
luoMingQ <luosanchun@foxmail.com>
2020-03-30 13:05:44 +0800

    first commit

---
+++ src/test/java/com/example/notebook/NotebookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.notebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotebookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
